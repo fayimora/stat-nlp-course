b4f6456b257e290df5bf69d9f579fc3d606a59ee
Fayimora Femi-Balogun <fayi@fayimora.com>
2014-12-01 10:28:49 +0000

    Add NaiveBayes Implementation

---
+++ statnlp-assignments/src/main/scala/uk/ac/ucl/cs/mr/assignment2/NaiveBayes.scala
@@ -0,0 +1,66 @@
+package uk.ac.ucl.cs.mr.assignment2
+
+import ml.wolfe.nlp.Token
+import uk.ac.ucl.cs.mr.assignment2.LoadData.FeatureTemplate
+import scala.collection.mutable.{ListBuffer, HashMap}
+import cc.factorie.la.{GrowableSparseTensor1 => SparseVector}
+
+/**
+ * Created by fayimora on 30/11/14.
+ */
+class NaiveBayes(labelTokens: HashMap[String, ListBuffer[Token]],
+                 featureTemplates: Seq[FeatureTemplate]) {
+
+  val candidate = if(labelTokens.keys.size > 3) "trigger" else "argument"
+  val vocabulary = labelTokens.values.flatten.toSeq
+
+  def prob(token: Token, trueLabels: Seq[String]): Tuple2[String, Double] = {
+    val pLabels = trueLabels map probLabel
+    val pWordLabels = trueLabels.map(l => probTokenLabel(token, l))
+    val pFeatLabels = trueLabels map probFeatLabel
+
+    val probabilities: Seq[Double] = for(i <- 0 to trueLabels.size-1) yield pLabels(i) * pWordLabels(i) * pFeatLabels(i)
+    val idx = probabilities.toSeq.zipWithIndex.maxBy(_._1)._2
+    (trueLabels(idx), probabilities(idx))
+  }
+
+  /**
+   * This function calculates the probability of a given label.
+   *
+   * @param label is the label of either the triggers or arguments. It could be Regulation, None, Theme, e.t.c
+   * @return the probability of the argument label
+   */
+  def probLabel(label: String): Double =
+    labelTokens(label).size / labelTokens.values.map(_.size).sum.toDouble
+
+  /**
+   * This function calculates the probability of a token given a label
+   *
+   * @param token the token whose probability we are interested in
+   * @param label the label whose prior we want to use
+   * @return the probability that of a word given a label
+   */
+  def probTokenLabel(token: Token, label: String): Double =
+    labelTokens(label).count(_.word == token.word)+1 / labelTokens(label).size.toDouble
+
+  /**
+   * This function returns the probability of features for a given label
+   * @param label label used to calculate the probability
+   * @return probability of features given a label
+   */
+  def probFeatLabel(label: String): Double = {
+    val tokens = labelTokens(label)
+    var prod = 1.0
+    for(ft <- featureTemplates) {
+      val tempVec = new SparseVector(Nil)
+      var i = 0
+      for(token <- tokens) {
+        tempVec += (i, ft(token, label, candidate))
+        i = i+1
+      }
+      val p = tempVec.sum/tokens.size
+      prod = prod * p
+    }
+    prod
+  }
+}
