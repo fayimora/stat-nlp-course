4e95383be9957b47832a354e555f12aece13b0f0
Fayimora Femi-Balogun <fayi@fayimora.com>
2014-12-02 12:03:31 +0000

    Fix precision() and add new accuracy function for a given label

--- statnlp-assignments/src/main/scala/uk/ac/ucl/cs/mr/assignment2/Evaluator.scala
+++ statnlp-assignments/src/main/scala/uk/ac/ucl/cs/mr/assignment2/Evaluator.scala
@@ -9,11 +9,12 @@ class Evaluator(predictedLabels: Seq[String], trueLabels: Seq[String]) {
   def precision(label: String): Double = {
     var (correctPredsCount, predCounts) = (0, 0)
     for((pred, gold) <- zippedLabels) {
-      if (pred == label)
+      if(pred != label) {
         if (pred == gold) correctPredsCount += 1
-      else predCounts += 1
+        predCounts += 1
+      }
     }
-    correctPredsCount/predCounts.toDouble
+    (correctPredsCount/predCounts.toDouble) * 100
   }
 
   def recall(label: String): Double = {
@@ -22,9 +23,22 @@ class Evaluator(predictedLabels: Seq[String], trueLabels: Seq[String]) {
       if(pred == gold && pred != label) correctPredsCount += 1
       if(gold != label) goldCounts += 1
     }
-    correctPredsCount/goldCounts.toDouble
+    (correctPredsCount/goldCounts.toDouble) * 100// precision(label)
   }
 
   def f1Measure(label: String): Double =
     2 * ( (precision(label) * recall(label)) / (precision(label) + recall(label)) )
+
+  def accuracy(): Double =
+    (zippedLabels.count(t => t._1 == t._2) / zippedLabels.size.toDouble) * 100
+
+  def accuracy(label: String): Double = {
+    var nLabels = 0
+    var nCorrectLabels = 0
+    for((pred, gold) <- zippedLabels if gold == label) {
+      if(pred==gold) nCorrectLabels += 1
+      nLabels += 1
+    }
+    (nCorrectLabels/nLabels.toDouble) * 100
+  }
 }
